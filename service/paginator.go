f2691dbbf009c0e9670e2c36fe5dd3c7e5e245f4
xiaocairen <wbhazz@163.com>
2020-06-02 11:25:44 +0800

    add LoadPageTargets(target interface{})

--- service/paginator.go
+++ service/paginator.go
@@ -41,9 +41,14 @@ func (p *Paginator) GetTotal() int64 {
 
 // target must be ptr to struct,
 // return []interface{} will be a targets slice.
-func (p *Paginator) GetCurPageTargets() ([]interface{}, error) {
+func (p *Paginator) GetPageTargets() ([]interface{}, error) {
 	p.Selection.Limit = msql.LimitOffset(uint64(p.calcOffset()), uint64(p.PerSize))
 	return p.Conn.Select(*p.Selection).Query().ScanStructAll(p.target)
+}
+
+func (p *Paginator) LoadPageTargets(target interface{}) ([]interface{}, error) {
+	p.Selection.Limit = msql.LimitOffset(uint64(p.calcOffset()), uint64(p.PerSize))
+	return p.Conn.Select(*p.Selection).Query().ScanStructAll(target)
 }
 
 func (p *Paginator) QueryResults() *rows {
