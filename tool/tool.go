caa173b3b7575fafc6e7654c8999a8997c540db1
xiaocairen <wbhazz@163.com>
2020-06-12 13:44:08 +0800

    change CheckImageFormat() check jpg

--- tool/tool.go
+++ tool/tool.go
@@ -714,8 +714,6 @@ func CheckImageFormat(file string) (int, error) {
 		return IMAGE_TYPE_UNKNOWN, fmt.Errorf("can't read image")
 	}
 
-	fmt.Printf("0x%x 0x%x 0x%x 0x%x\n", buf[0], buf[1], buf[2], buf[3])
-
 	if buf[0] == 0xff && buf[1] == 0xd8 && buf[2] == 0xff { // jpg
 		return IMAGE_TYPE_JPEG, nil
 	} else if buf[0] == 0x89 && buf[1] == 0x50 && buf[2] == 0x4e && buf[3] == 0x47 && buf[4] == 0x0d && buf[5] == 0x0a && buf[6] == 0x1a && buf[7] == 0x0a {
