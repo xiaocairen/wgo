3e651ce398350094643c3d464746391b0f428889
xiaocairen <wbhazz@163.com>
2020-06-12 12:55:36 +0800

    add CheckImageFormat()

--- tool/tool.go
+++ tool/tool.go
@@ -690,3 +690,38 @@ func pkcs7UnPadding(origData []byte) []byte {
 	unpadding := int(origData[length-1])
 	return origData[:(length - unpadding)]
 }
+
+const (
+	IMAGE_TYPE_UNKNOWN = -1
+	IMAGE_TYPE_JPEG    = 1
+	IMAGE_TYPE_PNG     = 2
+	IMAGE_TYPE_GIF     = 3
+	IMAGE_TYPE_BMP     = 4
+)
+
+func CheckImageFormat(file string) (int, error) {
+	f, e := os.OpenFile(file, os.O_RDONLY, 0644)
+	if e != nil {
+		return IMAGE_TYPE_UNKNOWN, e
+	}
+	defer f.Close()
+
+	buf := make([]byte, 8)
+	if n, e := f.Read(buf); e != nil {
+		return IMAGE_TYPE_UNKNOWN, e
+	} else if n != 8 {
+		return IMAGE_TYPE_UNKNOWN, fmt.Errorf("can not read image")
+	}
+
+	if buf[0] == 0xff && buf[1] == 0xd8 && buf[2] == 0xff && buf[3] == 0xe1 { // jpg
+		return IMAGE_TYPE_JPEG, nil
+	} else if buf[0] == 0x89 && buf[1] == 0x50 && buf[2] == 0x4e && buf[3] == 0x47 && buf[4] == 0x0d && buf[5] == 0x0a && buf[6] == 0x1a && buf[7] == 0x0a {
+		return IMAGE_TYPE_PNG, nil
+	} else if buf[0] == 0x47 && buf[1] == 0x49 && buf[2] == 0x46 && buf[3] == 0x38 && buf[4] == 0x39 && buf[5] == 0x61 {
+		return IMAGE_TYPE_GIF, nil
+	} else if buf[0] == 0x42 && buf[1] == 0x4d {
+		return IMAGE_TYPE_BMP, nil
+	}
+
+	return IMAGE_TYPE_UNKNOWN, fmt.Errorf("unknown image format")
+}
